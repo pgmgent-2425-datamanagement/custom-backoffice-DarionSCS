77f7513b9ba85a66a89bdbdee0920435343175e7
Darion <daria.habibi@student.arteveldehs.be>
2024-10-24 15:49:57 +0200

    header/footer and tailwind implementation

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./views/**/*.php", "./public/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
